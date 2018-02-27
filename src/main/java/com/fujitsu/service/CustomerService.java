3c0e7872c2e91df2ed20942c0e5db2b759e3a3ad
Maciej Solinski <Maciej.Solinski@ts.fujitsu.com>
2018-02-27 14:10:40 +0100

    Add access to application arguments in service component

--- src/main/java/com/fujitsu/service/CustomerService.java
+++ src/main/java/com/fujitsu/service/CustomerService.java
@@ -1,12 +1,22 @@
 package com.fujitsu.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService {
 
+    private final ApplicationArguments applicationArguments;
+
+    @Autowired
+    public CustomerService(ApplicationArguments applicationArguments) {
+        this.applicationArguments = applicationArguments;
+    }
+
     public String message() {
-        return "Hello world!!!";
+        String message = "Hello world! " + " App arguments: " + applicationArguments.getNonOptionArgs();
+        return message;
     }
 
 }
